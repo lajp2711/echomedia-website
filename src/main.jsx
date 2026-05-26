0ffc1692f953a76eaa0e619474b7db9eea208dff
Shannon Bray (NC) <108833174+ShannonBrayNC@users.noreply.github.com>
2026-05-26 04:32:47 -0400

    Add Lantern simulator trigger to React entrypoint

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import LanternCrisisTrigger from "./components/easter-eggs/LanternCrisisTrigger.jsx";
 import "./styles.css";
 
-createRoot(document.getElementById("root")).render(
+const container = document.getElementById("root");
+
+createRoot(container).render(
   <React.StrictMode>
     <App />
+    <LanternCrisisTrigger />
   </React.StrictMode>
 );
